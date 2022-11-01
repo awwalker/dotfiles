e7bea7cda079960a2acfc7d83d3ba84077ac11ee
Aaron Walker <awwalker3@gmail.com>
2022-10-31 20:50:45 -0400

    [nvim]: move mappings into plugin files

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,18 +1,8 @@
 -- =============================
 --            MAPPINGS
 -- =============================
-local silent = { silent = true }
 local noremap = { noremap = true }
 
-local noremap_silent = { noremap = true, silent = true }
-
-function _G.filepath_and_lineno()
-  local path_and_no = string.format("%s:%s", vim.fn.expand("%:p"), vim.fn.line("."))
-  local subbed = string.gsub(path_and_no, "" .. "/", "")
-  vim.api.nvim_command(string.format('call setreg("+", "%s")', subbed, {}))
-end
-
--- NATIVE
 -- Folds.
 vim.api.nvim_set_keymap("n", "<space>", "za", noremap)
 -- Move the screen.
@@ -48,60 +38,6 @@ vim.api.nvim_set_keymap("n", "<c-l>", "<c-w>l", noremap)
 vim.api.nvim_set_keymap("n", "<c-h>", "<c-w>h", noremap)
 vim.api.nvim_set_keymap("n", "<c-p>", "<c-w>p", noremap)
 
--- Macros
-vim.api.nvim_set_keymap("n", "<leader>dn", '<cmd> r! date "+%Y-%m-%d %H:%M:%S" <CR>', noremap)
-
--- TABS
+-- Tabs
 vim.api.nvim_set_keymap("n", "<leader>tf", '<cmd> lua vim.cmd("tabedit %") <CR>', noremap) -- Pull buffer into new tab.
 vim.api.nvim_set_keymap("n", "<leader>tc", '<cmd> lua vim.cmd("tabclose") <CR>', noremap) -- Close current tab.
-
--- PLUGINS
-
--- DAP
-vim.api.nvim_set_keymap("n", "<leader>c", '<cmd> lua require"dap".continue() <CR>', silent)
-vim.api.nvim_set_keymap("n", "<leader>n", '<cmd> lua require"dap".step_over() <CR>', silent)
-vim.api.nvim_set_keymap("n", "<leader>b", '<cmd> lua require"dap".toggle_breakpoint() <CR>', silent)
-vim.api.nvim_set_keymap("n", "<leader>dr", '<cmd> lua require"dap".repl.open() <CR>', silent)
-vim.api.nvim_set_keymap("n", "<leader>si", '<cmd> lua require"dap".step_into() <CR>', silent)
-vim.api.nvim_set_keymap("n", "<leader>so", '<cmd> lua require"dap".step_out() <CR>', silent)
--- handled per language in ftplugin
--- vim.api.nvim_set_keymap("n", "<leader>dt", '<cmd> lua require"dap-python".test_method()<CR>', silent)
-
--- FUGITIVE
-vim.api.nvim_set_keymap("n", "<leader>gs", ":Git<CR>", noremap)
-vim.api.nvim_set_keymap("n", "<leader>gd", ":Gdiffsplit<CR>", noremap)
-vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push<CR>", noremap)
-vim.api.nvim_set_keymap("n", "<leader>gc", ":Git commit<CR>", noremap)
-
--- TELESCOPE
--- Searching
-vim.api.nvim_set_keymap("n", "<c-f>", "<cmd> Telescope find_files<CR>", noremap_silent)
-vim.api.nvim_set_keymap("n", "<leader>f", "<cmd> Telescope live_grep_args<CR>", noremap_silent)
-vim.api.nvim_set_keymap("n", "<c-b>", "<cmd> Telescope buffers<CR>", noremap_silent)
-vim.api.nvim_set_keymap("n", "<c-g>", "<cmd> Telescope grep_string<CR>", noremap_silent)
-vim.api.nvim_set_keymap("n", "<c-m>", "<cmd> Telescope marks<CR>", noremap_silent)
-vim.api.nvim_set_keymap("n", "<leader>yr", "<cmd> Telescope registers<CR>", noremap_silent)
-vim.api.nvim_set_keymap("n", "<leader>gb", "<cmd> Telescope git_branches<CR>", noremap_silent)
--- Lsp
-vim.api.nvim_set_keymap("n", "<leader>r", "<cmd> Telescope lsp_references<CR>", noremap_silent)
-vim.api.nvim_set_keymap("n", "<leader>ca", "<cmd> lua vim.lsp.buf.code_action()<CR>", noremap_silent)
-vim.api.nvim_set_keymap("n", "<leader>ae", "<cmd> Telescope diagnostics bufnr=0 <CR>", noremap_silent)
-vim.api.nvim_set_keymap("n", "<leader>Ae", "<cmd> Telescope diagnostics <CR>", noremap_silent)
-
--- CMD LINE
-vim.api.nvim_set_keymap(
-  "n",
-  "<localleader><localleader>",
-  "<cmd> lua require('fine-cmdline').open()<CR>",
-  noremap_silent
-)
-
--- CONJURE
-vim.api.nvim_set_keymap("n", "<localleader>rt", "<cmd> ConjureCljRunCurrentTest <CR>", noremap)
-vim.api.nvim_set_keymap("n", "<localleader>rf", "<cmd> ConjureCljRunCurrentNsTests <CR>", noremap)
-vim.api.nvim_set_keymap("n", "<localleader>le", "<cmd> ConjureCljLastException <CR>", noremap)
-
--- DADBOD
-vim.api.nvim_set_keymap("n", "<leader>du", ":DBUIToggle<CR>", noremap)
-vim.api.nvim_set_keymap("n", "<leader>df", ":DBUIFindBuffer<CR>", noremap)
-vim.api.nvim_set_keymap("n", "<leader>di", ":DBUILastQueryInfo<Cr>", noremap)
