e80d34e1f9ef46f37d73298280a4317d65f8d511
Aaron Walker <awwalker3@gmail.com>
2023-01-11 14:45:17 -0500

    [nvim]: better single quotes for clj

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,4 +1,11 @@
 -- =============================
 --          AUTOPAIRS
 -- =============================
-require("nvim-autopairs").setup({ enable_check_bracket_line = false })
+
+local autopairs = require("nvim-autopairs")
+local cond = require("nvim-autopairs.conds")
+
+autopairs.setup({ enable_check_bracket_line = false })
+-- remove add single quote on filetype scheme or lisp
+autopairs.get_rule("'")[1].not_filetypes = { "scheme", "lisp", "clojure" }
+autopairs.get_rule("'")[1]:with_pair(cond.not_after_text("["))
