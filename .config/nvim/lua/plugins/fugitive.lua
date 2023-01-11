4300e20292458bde07a6d15be6af83822faa1955
Aaron Walker <awwalker3@gmail.com>
2023-01-11 14:48:29 -0500

    [nvim]: better fugitive mappings and buffer support

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,6 +1,10 @@
 local noremap = { noremap = true }
 
-vim.api.nvim_set_keymap("n", "<leader>gs", ":Git<CR>", noremap)
 vim.api.nvim_set_keymap("n", "<leader>gd", ":Gdiffsplit<CR>", noremap)
-vim.api.nvim_set_keymap("n", "<leader>gp", ":Git push<CR>", noremap)
-vim.api.nvim_set_keymap("n", "<leader>gc", ":Git commit<CR>", noremap)
+vim.api.nvim_set_keymap("n", "<leader>gpf", ":Git push --force<CR>", noremap)
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("FugitiveSmartQ", { clear = true }),
+  pattern = { "fugitive-summary", "fugitiveblame", "fugitive" },
+  command = "nmap <buffer> q gq",
+})
