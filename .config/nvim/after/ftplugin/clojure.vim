173f23871bf2051a27c735384f09da33b2b4576d
Aaron Walker <awwalker3@gmail.com>
2023-01-11 14:44:46 -0500

    [nvim]: cleanup conjure and clojure

--- .config/nvim/after/ftplugin/clojure.vim
+++
@@ -1,9 +0,0 @@
-nmap <silent> <leader>b <localleader>h#dbg<esc>
-nmap <silent> <leader>di :ConjureCljDebugInit <CR>
-nmap <silent> <leader>c :ConjureCljDebugInput continue <CR>
-nmap <silent> <leader>si :ConjureCljDebugInput in <CR>
-nmap <silent> <leader>so :ConjureCljDebugInput out <CR>
-nmap <silent> <leader>cc :ConjureCljDebugInput continue-all <CR>
-nmap <silent> <leader>n :ConjureCljDebugInput next <CR>
-nmap <silent> <leader>l :ConjureCljDebugInput locals <CR>
-nmap <silent> <leader>i :ConjureCljDebugInput inspect-prompt <CR>
--- .config/nvim/lua/plugins/conjure.lua
+++ .config/nvim/lua/plugins/conjure.lua
@@ -1,10 +1,9 @@
 -- =============================
 --            CONJURE
 -- =============================
-vim.g.conjure = {
-  debug = true,
-}
+-- vim.g["conjure#debug"] = true
 
+-- autocmd BufNewFile conjure-log-* lua vim.diagnostic.disable(0)
 vim.api.nvim_create_autocmd("BufNewFile", {
   group = vim.api.nvim_create_augroup("ConjureLog", { clear = true }),
   pattern = "conjure-log-*",
@@ -12,10 +11,34 @@ vim.api.nvim_create_autocmd("BufNewFile", {
     vim.diagnostic.disable(0)
   end,
 })
+-- autocmd User ConjureEval if expand("%:t") =~ "^conjure-log-" | exec "normal G" | endif
+vim.api.nvim_create_autocmd("User", {
+  group = vim.api.nvim_create_augroup("ConjureEval", { clear = true }),
+  pattern = "ConjureEval",
+  callback = function(params)
+    if string.match(vim.api.nvim_buf_get_name(0), "conjure%-log%-") then
+      vim.api.nvim_exec([[normal G]], true)
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("ConjureSmartQ", { clear = true }),
+  pattern = "conjure-log-*",
+  command = "nmap <buffer> q gq",
+})
+
 -- ============================
 --           MAPPINGS
 -- ============================
 local noremap = { noremap = true }
-vim.api.nvim_set_keymap("n", "<localleader>rt", "<cmd> ConjureCljRunCurrentTest <CR>", noremap)
-vim.api.nvim_set_keymap("n", "<localleader>rf", "<cmd> ConjureCljRunCurrentNsTests <CR>", noremap)
+vim.api.nvim_set_keymap("n", "<localleader>rt", "<cmd> CcaNreplRunCurrentTest<CR>", noremap)
+vim.api.nvim_set_keymap("n", "<localleader>rf", "<cmd> CcaNreplRunTestsInTestNs<CR>", noremap)
 vim.api.nvim_set_keymap("n", "<localleader>le", "<cmd> ConjureCljLastException <CR>", noremap)
+vim.api.nvim_set_keymap("n", "<leader>di", "<cmd> ConjureCljDebugInit<CR>", noremap)
+vim.api.nvim_set_keymap("n", "<leader>c", "<cmd> ConjureCljDebugInput continue<CR>", noremap)
+vim.api.nvim_set_keymap("n", "<leader>si", "<cmd> ConjureCljDebugInput in<CR>", noremap)
+vim.api.nvim_set_keymap("n", "<leader>so", "<cmd> ConjureCljDebugInput out<CR>", noremap)
+vim.api.nvim_set_keymap("n", "<leader>l", "<cmd> ConjureCljDebugInput locals<CR>", noremap)
+vim.api.nvim_set_keymap("n", "<leader>n", "<cmd> ConjureCljDebugInput next<CR>", noremap)
+vim.api.nvim_set_keymap("n", "<leader>de", "<cmd> ConjureCljDebugInput eval<CR>", noremap)
