53818700bfd8de8775565f41fbc368d2f4bede95
Aaron Walker <awwalker3@gmail.com>
2025-03-26 09:28:59 -0400

    config

--- .config/nvim/lua/plugins/lsp/conform.lua
+++ .config/nvim/lua/plugins/lsp/conform.lua
@@ -3,42 +3,56 @@ local M = {
 		"stevearc/conform.nvim",
 		event = { "BufWritePre", "BufNewFile" },
 		cmd = { "ConformInfo" },
-		setup = { log_level = vim.log.levels.ERROR },
-		-- This will provide type hinting with LuaLS
-		---@module "conform"
-		opts = {
-			-- Define your formatters
-			formatters_by_ft = {
-				-- clojure = { "cljfmt " },
-				lua = { "stylua" },
-				markdown = { "markdownlint", "prettier" },
-				python = { "isort", "black" },
-				javascript = { "prettier", stop_after_first = true },
-				json = { "prettier" },
-			},
-			-- Set default options
-			default_format_opts = {
-				lsp_format = "fallback",
-			},
-			-- Set up format-on-save
-			format_on_save = { timeout_ms = 500 },
-			-- Customize formatters
-			formatters = {
-				prettier = {
-					options = {
-						json = "json",
-						markdown = "markdown",
-						["markdown.mdx"] = "mdx",
-					},
+		config = function()
+			require("conform").setup({
+				log_level = vim.log.levels.DEBUG,
+				-- This will provide type hinting with LuaLS
+				---@module "conform"
+				-- Define your formatters
+				formatters_by_ft = {
+					-- clojure = { "cljfmt " },
+					lua = { "stylua" },
+					markdown = { "prettier_md" }, --, "markdownlint" },
+					python = { "isort", "black" },
+					javascript = { "prettier", stop_after_first = true },
+					json = { "prettier" },
 				},
-				stylua = {
-					append_args = {
-						"--config-path",
-						vim.fn.expand("~/.config/stylua/stylua.toml"),
+				-- Set up format-on-save
+				format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+				-- Customize formatters
+				formatters = {
+					prettier = {
+						options = {
+							ft_parsers = {
+								json = "json",
+								["markdown.mdx"] = "mdx",
+							},
+						},
 					},
+					prettier_md = {
+						command = "prettier",
+						args = {
+							"--parser",
+							"markdown",
+							"--print-width",
+							"80",
+							"--prose-wrap",
+							"always",
+							"--stdin-filepath",
+							"$FILENAME",
+						},
+						-- "--prose-wrap", "always", "$FILENAME" },
+					},
+					stylua = {
+						append_args = {
+							"--config-path",
+							vim.fn.expand("~/.config/stylua/stylua.toml"),
+						},
+					},
+					markdown = {},
 				},
-			},
-		},
+			})
+		end,
 		init = function()
 			-- If you want the formatexpr, here is the place to set it
 			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
