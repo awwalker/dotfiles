3017a42085aeaca9e71deb9b3e385193ce0f174e
Aaron Walker <awwalker3@gmail.com>
2026-09-12 12:59:43 -0400

    [conjure]: test runs refresh namespaces

--- .config/nvim/lua/plugins/code/conjure.lua
+++ .config/nvim/lua/plugins/code/conjure.lua
@@ -23,11 +23,12 @@ local M = {
 			-- Show HUD REPL log at startup
 			vim.g["conjure#log#hud#enabled"] = false
 			-- vim.g["conjure#client#clojure#nrepl#connection#auto_repl#hidden"] = true
-			-- vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = false
+			vim.g["conjure#client#clojure#nrepl#eval#auto_require"] = true
 			vim.g["conjure#client#clojure#nrepl#connection#auto_repl#cmd"] = "clj -M:repl/conjure"
 			vim.g["conjure#client#clojure#nrepl#eval#raw_out"] = true
 			vim.g["conjure#client#clojure#nrepl#test#raw_out"] = true
 			vim.g["conjure#client#clojure#nrepl#test#runner"] = "clojure"
+			vim.g["conjure#client#clojure#nrepl#refresh#backend"] = "clj-reload"
 			-- vim.g["conjure#client#clojure#nrepl#test#call_suffix"] = '{:config-file "tests.edn"}'
 			vim.g["conjure#client#clojure#nrepl#eval#print_function"] = "cider.nrepl.pprint/pprint"
 			-- vim.g["conjure#log#hud#open_when"] = "log-win-not-visible"
@@ -68,8 +69,18 @@ local M = {
 			})
 			vim.api.nvim_set_keymap("n", "<leader>c", "<cmd> ConjureCljDebugInput continue<CR>", noremap)
 			vim.api.nvim_set_keymap("n", "<leader>n", "<cmd> ConjureCljDebugInput next<CR>", noremap)
-			vim.api.nvim_set_keymap("n", "<localleader>rt", "<cmd> ConjureCljRunCurrentTest<Cr>", noremap)
-			vim.api.nvim_set_keymap("n", "<localleader>rns", "<cmd> ConjureCljRunCurrentNsTests<Cr>", noremap)
+			vim.api.nvim_set_keymap(
+				"n",
+				"<localleader>rt",
+				"<cmd>ConjureCljRefreshChanged<CR><cmd>ConjureCljRunCurrentTest<CR>",
+				noremap
+			)
+			vim.api.nvim_set_keymap(
+				"n",
+				"<localleader>rns",
+				"<cmd>ConjureCljRefreshChanged<CR><cmd>ConjureCljRunCurrentNsTests<CR>",
+				noremap
+			)
 		end,
 	},
 	{ "Olical/nfnl", ft = "fennel" },
